fe9d358ccf9fdcac13faba1f3d3341a4e428b5ac
Marko Maric <marko@markomaric.me>
2022-08-26 23:54:21 +0200

    Implemented tailwindcss. Removed the final piece of stylesheet boilerplate.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./utils/router.js";
 
-import "./style.css";
+import "./styles/global.css";
 
 createApp(App).use(router).mount("#app");
