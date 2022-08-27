fa438693ba2190423fd88c08eaf6d4e423ab81e4
Marko Maric <marko@markomaric.me>
2022-08-27 13:26:39 +0200

    Added a prettier file for code formatting. Implemented the first version of the applications main header component.

---
+++ src/components/header/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header :class="headerClasses">
+    <div class="container mx-auto h-16 flex items-center gap-6">
+      <!-- Anilist Logo -->
+      <img src="https://anilist.co/img/icons/icon.svg" class="h-12" />
+
+      <!-- Main Navigation -->
+      <nav class="flex flex-1 justify-center gap-6">
+        <router-link to="/" :class="navigationItemClasses">Home</router-link>
+        <router-link to="/profile" :class="navigationItemClasses">Profile</router-link>
+        <router-link to="/anime-list" :class="navigationItemClasses">Anime List</router-link>
+        <router-link to="/manga-list" :class="navigationItemClasses">Manga List</router-link>
+        <router-link to="/browse" :class="navigationItemClasses">Browse</router-link>
+        <router-link to="/forum" :class="navigationItemClasses">Forum</router-link>
+      </nav>
+
+      <!-- Search & User Profile -->
+      <section>
+        <div class="flex flex-row items-center"></div>
+      </section>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  computed: {
+    /**
+     * The classes which are applied to the header.
+     */
+    headerClasses() {
+      return {
+        "h-16 bg-gray-900 select-none": true,
+        "transition-all duration-500": true,
+      };
+    },
+
+    /**
+     * The classes which are applied to the navigation items.
+     */
+    navigationItemClasses() {
+      return {};
+    },
+  },
+};
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Profile.vue</div>
+  <div>
+    <Header />
+  </div>
 </template>
+
+<script>
+import Header from "../components/header/Header.vue";
+
+export default {
+  name: "ProfileView",
+  components: {
+    Header,
+  },
+};
+</script>
